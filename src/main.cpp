a3e46880db348c7e3624942134a273b6fd27f579
RAhmedM <ahmed.mustafa8696@gmail.com>
2024-01-24 01:42:16 +0500

    commit of 23 Jan 2024

--- src/main.cpp
+++ src/main.cpp
@@ -23,32 +23,52 @@ void startRawMode()
 
     modified_terminos = original_terminos;
 
-    //we turn off the echo flag on each keypress
+    //we turn off the echo flag
     //turning of canonical mode
     //turning of ctrl + z and ctrl + c
+    //turning of ctrl + v
 
-    modified_terminos.c_lflag &= ~(ECHO | ICANON | ISIG);
+    modified_terminos.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
     
     //Disable Ctrl-S and Ctrl-Q
-    modified_terminos.c_iflag &= ~(IXON);
+    //Fix control M
+    //sends SIGINT signal
+    //disabling parity chechking
+    //turning off 8 bit stripping
+
+    modified_terminos.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+
+    //Dont need Output Prceesing for now
+    // //Turning off all output processing
+    modified_terminos.c_oflag &= ~(OPOST);
+
+    //Sets charachter size to 8 bits per byte
+    modified_terminos.c_cflag |= (CS8);
+
+    modified_terminos.c_cc[VMIN] = 0;
+    modified_terminos.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO,TCSAFLUSH ,&modified_terminos);
 }
 
 int main()
 {
+    cout << "--------------TurboEditor--------------" << "\r\n";
     startRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1 && c != 'q') == 1)
-    {
+    while (1) {
+        char c = '\0';
+        
+        //read(STDIN_FILENO, &c, 1);
+        if(!read(STDIN_FILENO, &c, 1)) continue;
+        
         if (iscntrl(c))
         {
-            cout << static_cast<int>(c) << endl;
+            cout << static_cast<int>(c) << "\r\n";
         } 
         else{
-            cout << c << " :: " << static_cast<int>(c) << endl;
+            cout << c << " :: " << static_cast<int>(c) << "\r\n";
         }
-    
+        if (c == 'q') break;
 
     }
     
